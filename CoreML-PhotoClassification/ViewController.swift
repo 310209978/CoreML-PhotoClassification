ca694094764efd34840fad99b9e2da823f769824
elaine <310209978@qq.com>
2020-04-20 10:22:58 +0800

    Initial Commit

---
+++ CoreML-PhotoClassification/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreML-PhotoClassification
+//
+//  Created by elaine on 2020/4/20.
+//  Copyright © 2020 yuri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
